29e93019106d9af53591dbcf5408643ef2572c29
Kristian Cross <etamin1985@yahoo.com>
2024-07-14 22:22:22 +0000

    modify html, add JavaScript block

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+let lon;
+let lat;
+let loc = document.queryselector(".location");
+let windir = document.queryselector(".wind direction");
+let winspd = document.queryselector(".wind speed");
+let beascl = document.queryselector(".beaufort scale");
+let seasta = document.queryselector(".sea state");
+let ovrcon = document.queryselector(".overall condition");
+
