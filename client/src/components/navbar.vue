4fc44e02bb566a4720456c531a91fc0c0e1d57c4
Kristian Cross <etamin1985@yahoo.com>
2024-10-08 20:28:26 +0000

    Add simple navigation bar & home view that displays the animated forecast component

---
+++ client/src/components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>UK Shipping Forecast</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn to="/" text>Home</v-btn>
+      <v-btn to="/about" text>About</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  };
+  </script>
+  
+  <style scoped>
+  .v-btn {
+    margin: 0 10px;
+  }
+  </style>
+  
---
+++ client/src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+      <AnimatedForecastDisplay />
+    </v-container>
+  </template>
+  
+  <script>
+  import AnimatedForecastDisplay from '../components/AnimatedForecastDisplay.vue';
+  
+  export default {
+    components: {
+      AnimatedForecastDisplay,
+    },
+  };
+  </script>
+  
