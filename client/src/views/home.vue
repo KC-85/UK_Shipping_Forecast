138442a3f999762f63f0402641ceba37a4fd3155
Kristian Cross <etamin1985@yahoo.com>
2024-10-08 21:01:51 +0000

    Add logic to home.vue for animated forecast. scroll forecast & interactive map rendering

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,16 +1,26 @@
 <template>
-    <v-container>
-      <AnimatedForecastDisplay />
-    </v-container>
-  </template>
-  
-  <script>
-  import AnimatedForecastDisplay from '../components/AnimatedForecastDisplay.vue';
-  
-  export default {
-    components: {
-      AnimatedForecastDisplay,
-    },
-  };
-  </script>
-  
+  <v-container>
+    <!-- Original Animated Forecast Display Component -->
+    <AnimatedForecastDisplay />
+
+    <!-- Scroll Triggered Animation Component for Forecasts -->
+    <ScrollForecast />
+
+    <!-- Interactive Map Component for Region-Based Forecasts -->
+    <InteractiveMap />
+  </v-container>
+</template>
+
+<script>
+import AnimatedForecastDisplay from '../components/AnimatedForecastDisplay.vue';
+import ScrollForecast from '../components/ScrollForecast.vue';
+import InteractiveMap from '../components/InteractiveMap.vue';
+
+export default {
+  components: {
+    AnimatedForecastDisplay,
+    ScrollForecast,
+    InteractiveMap,
+  },
+};
+</script>
