e4fd75410825a0077c6b9e33c1e3c03325589319
Kristian Cross <etamin1985@yahoo.com>
2024-10-08 20:34:09 +0000

    Add brief description about this app

---
+++ client/src/views/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+      <v-card>
+        <v-card-title>About This App</v-card-title>
+        <v-card-text>
+          <p>This is a hyper-advanced UK Shipping Forecast app built with the MEVN stack. It uses GSAP for animations and Vuetify for styling.</p>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About',
+  };
+  </script>
+  
