370392817590b2d3ac3a08b04bfd43fd7cee28be
Kristian Cross <etamin1985@yahoo.com>
2024-10-10 20:41:14 +0000

    Update code from vue 2 to vue 3

--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,11 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false;
-
-Vue.use(Vuetify);
-
-new Vue({
-  router,
-  store,
-  vuetify: new Vuetify(),
-  render: h => h(App),
-}).$mount('#app');
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(vuetify);
+app.mount('#app');
