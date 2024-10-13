7feb700629c4459fb7437965eb2db25dfb9e9497
Kristian Cross <etamin1985@yahoo.com>
2024-10-13 18:37:40 +0000

    Add websocketURL to force-use WSS

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   configureWebpack: {
-    entry: './client/src/main.js'  // Adjust this path if your main.js is elsewhere
+    entry: './client/src/main.js',  // Adjust this path if your main.js is elsewhere
   },
   devServer: {
-    allowedHosts: "all",  // This fixes the "Invalid Host Header" issue
-  }
+    allowedHosts: "all",  // Fixes "Invalid Host Header" issue
+    https: true,  // Enable HTTPS for the dev server
+    client: {
+      webSocketURL: 'wss://0.0.0.0:8080/ws',  // Force WebSocket to use WSS
+    },
+  },
 };
