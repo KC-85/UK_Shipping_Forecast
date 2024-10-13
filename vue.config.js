ebd52d3323a4bf2072b7d84dc5faf6c1b7769894
Kristian Cross <etamin1985@yahoo.com>
2024-10-13 18:26:12 +0000

    Fix invalid host issue

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
-// vue.config.js
 module.exports = {
-    configureWebpack: {
-      entry: './client/src/main.js'  // This points to the correct location of your main.js
-    }
-  };
-  
+  configureWebpack: {
+    entry: './client/src/main.js'  // Adjust this path if your main.js is elsewhere
+  },
+  devServer: {
+    allowedHosts: "all",  // This fixes the "Invalid Host Header" issue
+  }
+};
